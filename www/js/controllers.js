446f6b2eda84c03d5535c8a29bae8646e8eb172e
Alirio Pretel <aperoquemira@gmail.com>
2017-07-13 02:36:27 -0500

    registro-usuario

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,75 +1,57 @@
 angular.module('starter.controllers', [])
 
-.controller('LoginCtrl', [
-  '$scope', '$state', '$timeout', 'FirebaseDB',
-  function LoginCtrl($scope, $state, $timeout, FirebaseDB) {
-    console.log("Login Controller");
-    $scope.resr = false;
-    $scope.doLoginAction = function (_credentials) {
-      FirebaseDB.login(_credentials)
-      .then(function (authData) {
-        console.log("Logged in as:", authData.uid);
-        $state.go('tab.chats', {})
-      })
-      .catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.error("Authentication failed:", error);
-      });
-    }
+  .controller('LoginCtrl', [
+    '$scope', '$state', '$timeout', 'FirebaseDB',
+    function LoginCtrl($scope, $state, $timeout, FirebaseDB) {
+      console.log("Login Controller");
 
-    $scope.doCreateUserAction = function (_credentials) {
-      FirebaseDB.createUser(_credentials).then(function (authData) {
-        console.log("Logged in as:", authData);
-        $state.go('tab.chats', {})
-      }).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.error("Authentication failed:", error);
-      });
+      /**
+       *
+       */
+      $scope.doLoginAction = function (_credentials) {
 
-    }
+        FirebaseDB.login(_credentials).then(function (authData) {
+          console.log("Logged in as:", authData.uid);
+          $state.go('tab.chats', {})
+        }).catch(function (error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.error("Authentication failed:", error);
+          // ...
+        });
 
-    $scope.doResetPassword = function (_credentials) {
+      }
 
-      console.log("Reset data creds:", _credentials.email);
-      FirebaseDB.resetPassword(_credentials).then(function (authData) {
-        $scope.resr = true;
-        $scope.$apply();
-        console.log("Reset done");
-        //$state.go('tab.chats', {})
+      /**
+       *
+       */
+      $scope.doCreateUserAction = function (_credentials) {
 
-      }).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.error("Reset failed failed:", error);
-      });
+        FirebaseDB.createUser(_credentials).then(function (authData) {
+          console.log("Logged in as:", authData);
+          $state.go('tab.chats', {})
+        }).catch(function (error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.error("Authentication failed:", error);
+          // ...
+        });
 
-    }
+      }
+    }])
 
+.controller('DashCtrl', function($scope) {})
 
-
-
-
-  }])
-
-.controller('DashCtrl', function($scope) {
-
-})
-
-.controller('ChatsCtrl', function($scope, $timeout, Chats, FirebaseDB, $state) {
-
-  $scope.doLogout = function () {
-    $timeout(function () {
-      $state.go('login', {})
-    }, 1);
-    firebase.auth().signOut()
-  }
-
-
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
 
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
@@ -77,80 +59,12 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('ChatsCtrl', function($scope, $timeout, Chats, FirebaseDB, $state) {
-
-  $scope.doLogout = function () {
-    $timeout(function () {
-      $state.go('login', {})
-    }, 1);
-    firebase.auth().signOut()
-  }
-
-
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-
-.controller('ChatDetailCtrl', function($scope, $stateParams,$timeout, Chats, FirebaseDB, $state) {
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
-
-
-  $scope.doLogout = function () {
-    $timeout(function () {
-      $state.go('login', {})
-    }, 1);
-
-    firebase.auth().signOut()
-    console.log("Saliendo ...");
-
-  }
-})
-
-
-.controller('ChatDetailCtrl', function($scope, $stateParams,$timeout, Chats, FirebaseDB, $state) {
-  $scope.chat = Chats.get($stateParams.chatId);
-
-
-  $scope.doLogout = function () {
-    $timeout(function () {
-      $state.go('login', {})
-    }, 1);
-
-    firebase.auth().signOut()
-    console.log("Saliendo ...");
-
-  }
 })
 
 .controller('AccountCtrl', function($scope) {
-    $scope.settings = {
+  $scope.settings = {
     enableFriends: true
   };
-
-//
-  $scope.validateEmail = function(email) {
-    var pattern = new RegExp(/^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/);
-    return pattern.test(email);
-  };
-
-    $scope.validatePassword = function(pass) {
-      if(pass.length >7){
-        return true;
-      }
-      else return false;
-    };
-
-
-  $scope.doLogout = function () {
-    $timeout(function () {
-      $state.go('login', {})
-    }, 1);
-    console.log("Saliendo ...");
-    return firebase.auth().signOut()
-  };
-
 });
